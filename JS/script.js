b2d27e36bb87b377f3205d4e4b3379cce0dffcf6
jean elossy <jean.elossy@yahoo.com>
2021-08-01 20:17:35 +0200

    autre mise a jour des fichiers du projet

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,11 @@ document.getElementById("img__black").addEventListener("click", changeBlack);
 function changeBlack(){
   document.querySelector(".picture-large").innerHTML = `<img src="./RESSOURCES/IMAGES/noir-grand.png" alt="canette couleur noir" id="img__black">`;
   document.querySelector(".container__right").style.backgroundColor = "black";
-  spanVisible();
+  document.querySelectorAll(".img__bottom").forEach(element => element.addEventListener("click", spanVisible));
 }
 
 // faire apparaitre le trait noir en dessous des images cliquées.
-document.querySelectorAll(".img__bottom").forEach(element => element.addEventListener("click", spanVisible));
+
 function spanVisible(){
   this.children[2].style.opacity = "1";
 }
@@ -17,10 +17,11 @@ document.getElementById("img__blue__one").addEventListener("click", changeBlueOn
 function changeBlueOne(){
   document.querySelector(".picture-large").innerHTML = `<img src="./RESSOURCES/IMAGES/bleu-grand2.png" alt="canette couleur bleu">`;
   document.querySelector(".container__right").style.backgroundColor = " rgb(22, 22, 136)";
-  spanVisible();
+  document.querySelectorAll(".img__bottom").forEach(element => element.addEventListener("click", spanVisible));
+  
 }
 
-// Canette jaune
+// // Canette jaune
 document.getElementById("img__yellow").addEventListener("click", changeYellow);
 function changeYellow(){
   document.querySelector(".picture-large").innerHTML = `<img src="./RESSOURCES/IMAGES/jaune-grand.png" alt="canette couleur jaune" id="large__image">`;
@@ -32,8 +33,10 @@ document.getElementById("img__blue__two").addEventListener("click", changeBlueTw
 function changeBlueTwo(){
   document.querySelector(".picture-large").innerHTML = `<img src="./RESSOURCES/IMAGES/bleu-grand.png" alt="canette couleur bleu">`;
   document.querySelector(".container__right").style.backgroundColor = " rgb(22, 22, 136)";
-  spanVisible();
+  document.querySelectorAll(".img__bottom").forEach(element => element.addEventListener("click", spanVisible));
+  
 }
+
 // overlay canette bleue 1
 document.querySelector(".overlay__bleu__one").addEventListener("click", overlayHiddenblueOne);
 function overlayHiddenblueOne(){
@@ -62,11 +65,12 @@ function overlayHiddenblack(){
   changeBlack();
 }
 
+// document.querySelectorAll(".over").forEach(element => element.addEventListener("mouseover", mouseover));
+// function mouseover(){
+//  this.nextElementSibling.nextElementSibling.style.opacity = "1";
+// }
 
-
-
-document.querySelector(".overlay__bleu__one").addEventListener("mouseover", hoverBlue);
-function hoverBlue(){
- 
-}
-
+// document.querySelectorAll(".over").forEach(element => element.addEventListener("mouseout", mouseout));
+// function mouseout(){
+//  this.nextElementSibling.nextElementSibling.style.opacity = "0";
+// }

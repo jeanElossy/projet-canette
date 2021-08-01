d5ca15bfbd5cd0ecd18f1b98a93abfe5acc24c1e
jean elossy <jean.elossy@yahoo.com>
2021-08-01 18:29:16 +0200

    Autre modification dans les fichiers du projet

--- JS/script.js
+++ JS/script.js
@@ -3,14 +3,21 @@ document.getElementById("img__black").addEventListener("click", changeBlack);
 function changeBlack(){
   document.querySelector(".picture-large").innerHTML = `<img src="./RESSOURCES/IMAGES/noir-grand.png" alt="canette couleur noir" id="img__black">`;
   document.querySelector(".container__right").style.backgroundColor = "black";
-  // document.querySelectorAll(".img__bottom span").forEach(element => element.style.opacity = "1");
+  spanVisible();
 }
- 
+
+// faire apparaitre le trait noir en dessous des images cliquées.
+document.querySelectorAll(".img__bottom").forEach(element => element.addEventListener("click", spanVisible));
+function spanVisible(){
+  this.children[2].style.opacity = "1";
+}
+
 // Canette bleue 1
 document.getElementById("img__blue__one").addEventListener("click", changeBlueOne);
 function changeBlueOne(){
   document.querySelector(".picture-large").innerHTML = `<img src="./RESSOURCES/IMAGES/bleu-grand2.png" alt="canette couleur bleu">`;
   document.querySelector(".container__right").style.backgroundColor = " rgb(22, 22, 136)";
+  spanVisible();
 }
 
 // Canette jaune
@@ -25,6 +32,41 @@ document.getElementById("img__blue__two").addEventListener("click", changeBlueTw
 function changeBlueTwo(){
   document.querySelector(".picture-large").innerHTML = `<img src="./RESSOURCES/IMAGES/bleu-grand.png" alt="canette couleur bleu">`;
   document.querySelector(".container__right").style.backgroundColor = " rgb(22, 22, 136)";
+  spanVisible();
+}
+// overlay canette bleue 1
+document.querySelector(".overlay__bleu__one").addEventListener("click", overlayHiddenblueOne);
+function overlayHiddenblueOne(){
+  this.style.opacity = "0";
+  changeBlueOne();
 }
 
+// overlay canette yellow
+document.querySelector(".overlay__jaune").addEventListener("click", overlayHiddenYellow);
+function overlayHiddenYellow(){
+  this.style.opacity = "0";
+  changeYellow();
+}
+
+// overlay canette bleue 2
+document.querySelector(".overlay__bleu__two").addEventListener("click", overlayHiddenblueTwo);
+function overlayHiddenblueTwo(){
+  this.style.opacity = "0";
+  changeBlueTwo();
+}
+
+// overlay canette black
+document.querySelector(".overlay__black").addEventListener("click", overlayHiddenblack);
+function overlayHiddenblack(){
+  this.style.opacity = "0";
+  changeBlack();
+}
+
+
+
+
+document.querySelector(".overlay__bleu__one").addEventListener("mouseover", hoverBlue);
+function hoverBlue(){
+ 
+}
 
